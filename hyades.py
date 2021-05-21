0915a12cea90e3eb6b6c772a07a16753b1452ad4
redboxup <anudeep28.2000@gmail.com>
2021-05-21 12:06:37 +0530

    hyades vpd plot complete

--- hyades.py
+++ hyades.py
@@ -4,14 +4,41 @@
 
 #read the csv file into a pandas dataframe
 from astromodule import *
+from astropy.coordinates import Distance
+
 df = pd.read_csv("./database/hyades_3deg_photometric.csv",
 	usecols=['ra','dec','pmra','pmdec',
 	'parallax','phot_g_mean_mag','bp_rp','dr2_radial_velocity'])
-
 #creating a dataframe to plot the propermotion of the stars in field of view
-df1 = df.drop(columns=['ra','dec','parallax','phot_g_mean_mag','bp_rp',
+df1 = df.drop(columns=['phot_g_mean_mag','bp_rp',
 			'dr2_radial_velocity'])
 df1 = df1.dropna()
+
+#removing from the cluster field of view stars 
+
+#first to find the distance of all the stars
+df1_parallax0 = df1.parallax.to_list()
+df1_parallax1 = list(map(abs,df1_parallax0))
+d = Distance(parallax = df1_parallax1*u.mas)
+d = d.to_value()
+
+#adding the distance column to the dataframe
+df1['distance']=d
+
+#now removing all the rows with a distance >100 pc
+df1 = df1.drop(df1[df1.distance >100].index)
+print(df1)
+
+
+#plotting the pmra and pmdec without performing gmm
+def plot_pm(df):
+	y = df.pmdec.tolist()
+	x = df.pmra.tolist()
+	plt.plot(x,y, 'ro',markersize = 0.5,alpha =0.5)
+	plt.show()
+#plot_pm(df1)
+
+df2 = df1.drop(columns = ['parallax','distance','ra','dec'])
 
 #To run gaussian mixture model on the data
 def gmm_(df,n_components):
@@ -22,13 +49,17 @@ def gmm_(df,n_components):
 	labels = gmm.predict(X)
 	
 	plt.show()
-#	final_index = user_select(df,labels)
-#	return final_index 	
+	final_index = user_select(df,labels)
+	return final_index 	
 
-#df2 = df1.loc[(df['pmra']>60)&(df['pmdec']<0)]
 
-gmm_(df1,2)
+index = gmm_(df2,2)
 
+df3 = filter_cluster(df1,index)
+
+ax = plt.subplot()
+ax.quiver(df3.ra, df3.dec,df3.pmra,df3.pmdec,scale = 1000,color='k')
+plt.show()
 
 
 
